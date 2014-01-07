1ca4d972be13700cc56d632030e6f6cf07b9b8a4
Jonathan Tsang <chewymeister88@googlemail.com>
2014-01-07 20:00:39 +0000

    retrives columns and boxes. refactored

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,4 +1,5 @@
 class Grid
+  STARTING_POINTS = [0,3,6] * 3
   attr_reader :board
   def initialize puzzle
     @puzzle = puzzle
@@ -19,6 +20,14 @@ class Grid
 
   def retrieve_cells_at_column(number)
     @board.map {|row| row[number - 1]}
+  end
+
+  def retrieve_cells_at_box(number)
+    @board.slice(number - 1,3).transpose.slice(start_point(number),3).transpose.flatten
+  end
+  
+  def start_point(number)
+    STARTING_POINTS[number - 1]
   end
 end
 
--- spec/lib/grid_spec.rb
+++ spec/lib/grid_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Grid do
-    let(:easy_grid) { Grid.new("209703810410205607875196023306520100100004359047309068000400980924867001050000002") }
+    let(:easy_grid) { Grid.new('209703810410205607875196023306520100100004359047309068000400980924867001050000002') }
   context 'setup' do
     
     it 'should be an instance of Grid' do
@@ -9,30 +9,34 @@ describe Grid do
     end
 
     it 'assigns values to a list of new cells' do
-      expect(easy_grid.assign_values_to_cells.first.value).to eq "2"
+      expect(easy_grid.assign_values_to_cells.first.value).to eq '2'
     end
     
     it 'creates a board of cells with indices' do
       easy_grid.create_board
-      expect(easy_grid.board[0][0].value).to eq "2"
+      expect(easy_grid.board[0][0].value).to eq '2'
     end
 
     it 'is initialized with a board ready to be manipulated' do
       # no setup required
-      expect(easy_grid.board[0][0].value).to eq "2"
+      expect(easy_grid.board[0][0].value).to eq '2'
     end
   end
 
   context 'retrieve rows, columns, and boxes' do
     it 'retrieves a row of cells' do
-      expect(easy_grid.retrieve_cells_at_row(2).first.value).to eq "4"
+      expect(easy_grid.retrieve_cells_at_row(2).first.value).to eq '4'
       expect(easy_grid.retrieve_cells_at_row(1).count).to eq 9
     end    
 
     it 'retrieves a column of cells' do
-      expect(easy_grid.retrieve_cells_at_column(1).first.value).to eq "2"
+      expect(easy_grid.retrieve_cells_at_column(1).first.value).to eq '2'
     end
-  end
 
+    it 'retrieves a box of cells' do
+      expect(easy_grid.retrieve_cells_at_box(1).last.value).to eq '5'
+    end
+
+  end
 end
 
