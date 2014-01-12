951889afdf47e7125e0c3508055b4cf3b4c4b0e6
Jonathan Tsang <chewymeister88@googlemail.com>
2014-01-12 20:10:44 +0000

    extracted cell class into separate file

---
+++ lib/cell.rb
@@ -0,0 +1,40 @@
+class Cell 
+  attr_reader :value
+  attr_reader :neighbours
+  attr_reader :candidates
+  def initialize(value)
+    @value = value
+    @neighbours = []
+    @candidates = ['1','2','3','4','5','6','7','8','9']
+  end
+
+  def solve_using(neighbours)
+    @neighbours = neighbours.map(&:value)
+    attempt_solution unless solved?
+  end
+
+  def attempt_solution
+    eliminate_candidates
+    assign_candidate_to_value if one_candidate_left? 
+  end
+
+  def eliminate_candidates
+    @candidates -= @neighbours
+  end
+
+  def assign_candidate_to_value
+    @value = @candidates.pop 
+  end
+  
+  def one_candidate_left?
+    @candidates.count == 1
+  end
+
+  def solved?
+    @value != '0'
+  end
+
+  def assume(candidate)
+    @value = candidate
+  end
+end
--- lib/grid.rb
+++ lib/grid.rb
@@ -1,3 +1,5 @@
+require_relative './cell'
+
 class Grid
   ROW_STARTING_POINT = [0, 0, 0, 3, 3, 3, 6, 6, 6]
   STARTING_POINT = [0,3,6] * 3
@@ -126,46 +128,5 @@ class Grid
       end
     puts "|\n-------------------------------------"
     end
-  end
-end
-
-class Cell 
-  attr_reader :value
-  attr_reader :neighbours
-  attr_reader :candidates
-  def initialize(value)
-    @value = value
-    @neighbours = []
-    @candidates = ['1','2','3','4','5','6','7','8','9']
-  end
-
-  def solve_using(neighbours)
-    @neighbours = neighbours.map(&:value)
-    attempt_solution unless solved?
-  end
-
-  def attempt_solution
-    eliminate_candidates
-    assign_candidate_to_value if one_candidate_left? 
-  end
-
-  def eliminate_candidates
-    @candidates -= @neighbours
-  end
-
-  def assign_candidate_to_value
-    @value = @candidates.pop 
-  end
-  
-  def one_candidate_left?
-    @candidates.count == 1
-  end
-
-  def solved?
-    @value != '0'
-  end
-
-  def assume(candidate)
-    @value = candidate
   end
 end
