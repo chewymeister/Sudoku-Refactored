113618cef95b685be7fb6c4aafb3f021fd142a24
Jonathan Tsang <chewymeister88@googlemail.com>
2014-01-13 15:44:31 +0000

    cell_spec.rb tests candidate elimination

--- spec/lib/cell_spec.rb
+++ spec/lib/cell_spec.rb
@@ -12,15 +12,33 @@ describe Cell do
     it 'should be initialized with a set of candidates' do
       expect(solved_cell.candidates).to eq ['1','2','3','4','5','6','7','8','9']
     end
+  end
 
+  context 'neighbours' do
     it 'should assign neighbours when given a set of neighbours by grid' do
-      neighbour_one = Cell.new('1')
-      neighbour_two = Cell.new('2')
-      neighbour_three = Cell.new('3')
-      neighbours = [neighbour_one, neighbour_two, neighbour_three]
-      unsolved_cell.assign(neighbours)
+      assign_neighbours_to(unsolved_cell, 3)
 
       expect(unsolved_cell.neighbours).to eq ['1','2','3']
     end
   end
+
+  context 'candidates' do
+    it 'should eliminate candidates using its neighbours' do
+      assign_neighbours_to(unsolved_cell, 3)
+      unsolved_cell.eliminate_candidates
+
+      expect(unsolved_cell.candidates).to eq ['4','5','6','7','8','9']
+    end
+  end
+
+  def assign_neighbours_to(cell, neighbour_count)
+    neighbours = []
+    1.upto(neighbour_count) { |n| neighbours << neighbour(n) }
+    cell.assign(neighbours) 
+  end
+
+  def neighbour(number)
+    Cell.new(number.to_s)
+  end
 end
+
